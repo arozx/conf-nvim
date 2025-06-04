0627fd09df42f516f33ae0f5b60e384184f5748f
arozx <arozx@disr.it>
2025-06-04 19:12:21 +0100

    switch vim.cmd.colorscheme to catppuccin-mocha from frappe

--- init.lua
+++ init.lua
@@ -894,7 +894,7 @@ require('lazy').setup({
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
-      vim.cmd.colorscheme 'catppuccin-frappe'
+      vim.cmd.colorscheme 'catppuccin-mocha'
       -- vim.cmd.colorscheme 'tokyonight-moon'
     end,
   },
