6b5ef8e607dc6a0557ba828335cd242d7adcdb19
arozx <arozx@disr.it>
2025-08-30 17:28:37 +0100

    run c binaries with full path to avoid issues in same dir

--- lua/custom/runfile/init.lua
+++ lua/custom/runfile/init.lua
@@ -1,14 +1,14 @@
 local M = {}
 
 function M.run_file()
-  local file = vim.fn.expand '%:p'
+  local file = vim.fn.fnamemodify(vim.fn.expand '%:p', ':p')
   local ext = vim.fn.expand '%:e'
 
   if ext == 'py' then
     vim.cmd('split | terminal python3 ' .. file)
   elseif ext == 'c' then
     local out = vim.fn.expand '%:r'
-    vim.cmd('split | terminal gcc ' .. file .. ' -o ' .. out .. ' && ' .. out)
+    vim.cmd('split | terminal gcc ' .. file .. ' -o ' .. out .. ' && ' .. vim.fn.fnamemodify(out, ':p'))
   elseif ext == 'lua' then
     vim.cmd('split | terminal lua ' .. file)
   else
